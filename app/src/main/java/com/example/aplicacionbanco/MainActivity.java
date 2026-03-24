3ed487581cfbfb5e32029710a792b461edc0720b
Jafet <kevinkevinjafetkevin2016@gmail.com>
2026-03-23 23:00:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/aplicacionbanco/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.aplicacionbanco;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.datospersonales);
+
+         setContentView(R.layout.domicilio);
+
+        // setContentView(R.layout.ingresos);
+    }
+}
